f8c84e161411b2b971e1e7ec5fddec5de727a6f2
ezemarvelouschijioke-hue <ezemarvelouschijioke@gmail.com>
2026-03-25 20:16:24 +0100

    Delete js directory

--- js/core/admin-secret.js
+++
@@ -1,6 +0,0 @@
-const ADMIN_SECRET = {
-
-    token: 'a7f9e2b1c8d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8',
-
-    passwordHash: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' // "password" hashed
-};
